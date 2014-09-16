f7f8047cbf9ae0a1aedf7cbebc7d2ba311215638
unknown <edz@SELT00381.tobii.intra>
2014-09-16 16:44:28 +0200

    starting the stats file

---
+++ stats_file_summary.py
@@ -0,0 +1,20 @@
+#Open file summary file .csv
+#TODO
+
+#Read the file, headers and relevant columns:
+''' File structure:
+    0 - General_comments
+    1 - Participant
+    2 - Included_ERP
+    3 - GC
+    4 - Test
+    5 - Video
+    6 - EEG
+    7 -Cal
+    8 - Gaze
+    9 - Onset_V
+    10 - Onset_P
+    11 - MatlabF
+    12 - Other comments
+'''
+
