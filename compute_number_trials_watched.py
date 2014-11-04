523fe0fe46f51a81274fb3af207e6197782c67c7
Estefania Dominguez <esdomar@gmail.com>
2014-11-04 11:23:12 +0100

    latest version used for analysis number of trials watched

--- compute_number_trials_watched.py
+++ compute_number_trials_watched.py
@@ -1,4 +1,5 @@
 from os import listdir, sep
+from open_GC_files import open_file_summary
 import numpy as np
 '''
     Open file per participant and save in a txt number of trials watched
@@ -13,9 +14,13 @@ def find_files(path, name):
     return nfile
 
 
-main_path = 'G:\Claire Project\Data\Participants'
+main_path = 'C:\GC data\Participant_data'
 #Open a file to write the results to a txt file
-f = open('number_of_trials_watched.txt', 'w')
+f = open('C:\Users\edz\OneDrive @ Tobii Technology AB\PhD\Chapter 2 - Gaze Contingency\Analysis\Results\partial results/number_of_trials_watched.txt', 'w')
+f.write('Participant' + '\t' + 'Included GC' + '\t' + 'GC' + '\t' + 'Number of trials' + '\n')
+
+file_summary = open_file_summary()
+
 number_of_trials = []
 
 for participant in listdir(main_path):
@@ -30,14 +35,14 @@ for participant in listdir(main_path):
         continue
 
     #import pdb; pdb.set_trace()
-    onset_data=[]
     onset_data = np.loadtxt(part_path + sep + onset_file_name, dtype=int,  delimiter="\t", skiprows=1)
-
 
     number_of_trials.append(len(onset_data))
     print participant, onset_file_name, len(onset_data)
 
+    participant_row = [row for row in file_summary if row[1] == int(participant[1:])][0]
+
     #write it to a file
-    f.write(participant + "\t" + str(number_of_trials[-1]) + "\n")
+    f.write(participant[1:] + "\t" + str(participant_row[3]) + "\t" + str(participant_row[4]) + "\t" + str(number_of_trials[-1]) + "\n")
         
 f.close()
